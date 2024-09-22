c7b1fa7872638b27238139c50e92c02f15aaf0d7
Elias85974 <128708960+Elias85974@users.noreply.github.com>
2024-09-22 16:55:43 -0300

    Application now running with postgres

---
+++ src/main/java/com/services/group4/permission/model/SnippetUser.java
@@ -0,0 +1,54 @@
+package com.services.group4.permission.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table
+public class SnippetUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userID;
+    @Column(nullable = false, unique = true)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    public SnippetUser() {
+    }
+
+    public SnippetUser(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public Long getUserID() {
+        return userID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
