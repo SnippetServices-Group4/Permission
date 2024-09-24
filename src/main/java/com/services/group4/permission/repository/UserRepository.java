2b6000bee5d8d384a49a802e3fb2ed84b74e39dd
JuliIglesias <jiglesias@mail.austral.edu.ar>
2024-09-24 15:42:59 -0300

    endpoints and UserTests for  them.

--- src/main/java/com/services/group4/permission/repository/UserRepository.java
+++ src/main/java/com/services/group4/permission/repository/UserRepository.java
@@ -4,6 +4,9 @@ import com.services.group4.permission.model.SnippetUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<SnippetUser, Long> {
+  Optional<SnippetUser> findByUsername(String username);
 }
