bbbfb6ccca61096c33b7ae6695ee21da846e6264
NachoCor2007 <incorrearodriguez@mail.austral.edu.ar>
2024-09-24 13:57:58 -0300

    Tested own external communications endpoints.

---
+++ src/test/java/com/services/group4/permission/communication/ModulesCommunicationTest.java
@@ -0,0 +1,34 @@
+package com.services.group4.permission.communication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ModulesCommunicationTest {
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Test
+  void testOwnParserCommunication() throws Exception {
+    this.mockMvc.perform(get("/test/parser/communication"))
+      .andExpect(status().isOk())
+      .andExpect(jsonPath("$.source").value("Permission"))
+      .andExpect(jsonPath("$.message").value("Communication from Parser to Permission works!"));
+  }
+
+  @Test
+  void testOwnSnippetCommunication() throws Exception {
+    this.mockMvc.perform(get("/test/snippet/communication"))
+      .andExpect(status().isOk())
+      .andExpect(jsonPath("$.source").value("Permission"))
+      .andExpect(jsonPath("$.message").value("Communication from Snippet to Permission works!"));
+  }
+}
