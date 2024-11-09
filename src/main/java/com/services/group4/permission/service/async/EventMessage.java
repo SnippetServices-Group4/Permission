af876a70f4f0ef143d6c1c6865eb9827e6e92063
NachoCor2007 <incorrearodriguez@mail.austral.edu.ar>
2024-11-09 14:17:38 -0300

    Created Redis event record.

---
+++ src/main/java/com/services/group4/permission/service/async/EventMessage.java
@@ -0,0 +1,4 @@
+package com.services.group4.permission.service.async;
+
+public record EventMessage(Long snippetId, String rules) {
+}
---
+++ src/main/java/com/services/group4/permission/service/async/LintEventProducer.java
@@ -0,0 +1,44 @@
+package com.services.group4.permission.service.async;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.connection.stream.ObjectRecord;
+import org.springframework.data.redis.connection.stream.StreamRecords;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Component
+public class LintEventProducer {
+  private final String streamKey;
+  private final RedisTemplate<String, String> redis;
+
+  @Autowired
+  public LintEventProducer(
+      @Value("${stream.lint.key}") String streamKey, @NotNull RedisTemplate<String, String> redis) {
+    this.streamKey = streamKey;
+    this.redis = redis;
+  }
+
+  public void emit(EventMessage product) {
+    ObjectRecord<String, EventMessage> result =
+        StreamRecords.newRecord().ofObject(product).withStreamKey(streamKey);
+
+    redis.opsForStream().add(result);
+  }
+
+  public void publishEvent(Long userId, Map<String, Object> jsonPayload) {
+    ObjectMapper mapper = new ObjectMapper();
+    try {
+      String jsonPayloadString = mapper.writeValueAsString(jsonPayload);
+      EventMessage product = new EventMessage(userId, jsonPayloadString);
+      emit(product);
+    } catch (Exception e) {
+      System.err.println("Error serializing jsonPayload: " + e.getMessage());
+    }
+  }
+}
