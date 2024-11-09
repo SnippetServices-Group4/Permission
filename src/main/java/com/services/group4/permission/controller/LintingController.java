62436d15f95f76ee7e221d0067b00169bade5b22
NachoCor2007 <incorrearodriguez@mail.austral.edu.ar>
2024-11-09 14:18:30 -0300

    Created a Linting Controller and Service.

---
+++ src/main/java/com/services/group4/permission/controller/LintingController.java
@@ -0,0 +1,23 @@
+package com.services.group4.permission.controller;
+
+import com.services.group4.permission.dto.UpdateRulesRequestDto;
+import com.services.group4.permission.service.LintingService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/linting")
+public class LintingController {
+  private final LintingService lintingService;
+
+  public LintingController(LintingService lintingService) {
+    this.lintingService = lintingService;
+  }
+
+  @PostMapping("/update/rules")
+  public void updateRulesAndLint(@RequestBody UpdateRulesRequestDto req) {
+    lintingService.updateRules(req);
+  }
+}
---
+++ src/main/java/com/services/group4/permission/service/LintingService.java
@@ -0,0 +1,11 @@
+package com.services.group4.permission.service;
+
+import com.services.group4.permission.dto.UpdateRulesRequestDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LintingService {
+  public void updateRules(UpdateRulesRequestDto req) {
+
+  }
+}
