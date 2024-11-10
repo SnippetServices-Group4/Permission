aecfa5c58f7fa03ea1144e731a6a8c17b5027548
JuliIglesias <jiglesias@mail.austral.edu.ar>
2024-11-10 16:07:13 -0300

    String userId process

--- src/main/java/com/services/group4/permission/service/ValidationService.java
+++ src/main/java/com/services/group4/permission/service/ValidationService.java
@@ -1,16 +1,12 @@
 package com.services.group4.permission.service;
 
-import com.services.group4.permission.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ValidationService {
 
-  // FUTURE: Check if the user is valid on AUTH0
-  @Autowired private UserRepository userRepository;
-
+  // TODO: Check if the user is valid on AUTH0
   public boolean isUserIdValid(String userId) {
-    return userRepository.existsById(userId);
+    return true;
   }
 }
