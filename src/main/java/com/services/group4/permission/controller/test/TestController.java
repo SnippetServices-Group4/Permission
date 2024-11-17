c86fc4497df6c6ac324fcf75d28a0fec948a2fa3
Elias85974 <128708960+Elias85974@users.noreply.github.com>
2024-11-17 17:36:30 -0300

    Test controller going back to not exposing anything

--- src/main/java/com/services/group4/permission/controller/test/TestController.java
+++ src/main/java/com/services/group4/permission/controller/test/TestController.java
@@ -1,21 +1,11 @@
 package com.services.group4.permission.controller.test;
 
 import com.services.group4.permission.model.communication.CommunicationMessage;
-import com.services.group4.permission.service.TokenService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/test")
 public class TestController {
-
-  private final RestTemplate restTemplate;
-
-  @Autowired
-  public TestController(RestTemplate restTemplate) {
-    this.restTemplate = restTemplate;
-  }
 
   @GetMapping("/ping")
   public String index() {
@@ -31,16 +21,5 @@ public class TestController {
   public CommunicationMessage testSnippetCommunication() {
     return new CommunicationMessage(
         "Permission", "Communication from Snippet to Permission works!");
-  }
-
-  @GetMapping("/getToken")
-  public String getToken() {
-    return new TokenService(restTemplate).getAccessToken();
-  }
-
-  @GetMapping("/getAllUsers")
-  public String getAllUsers() {
-
-    return restTemplate.getForObject("http://localhost:8081/user", String.class);
   }
 }
