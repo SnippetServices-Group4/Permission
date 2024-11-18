fd9f82ee1616be1dad0a61724aa1c586908339ca
NachoCor2007 <incorrearodriguez@mail.austral.edu.ar>
2024-11-18 16:22:53 -0300

    AllArgsConstructor

--- src/main/java/com/services/group4/permission/dto/FormatRulesDto.java
+++ src/main/java/com/services/group4/permission/dto/FormatRulesDto.java
@@ -1,10 +1,12 @@
 package com.services.group4.permission.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
+@AllArgsConstructor
 public class FormatRulesDto {
   private boolean spaceBeforeColon;
   private boolean spaceAfterColon;
