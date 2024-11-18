d3e42cb2db2aac53648c2208cf7625ae7b422c3d
Agustin <almartinez1@mail.austral.edu.ar>
2024-11-17 22:33:42 -0300

    id correlation - logs

--- src/main/java/com/services/group4/permission/config/RestTemplateConfig.java
+++ src/main/java/com/services/group4/permission/config/RestTemplateConfig.java
@@ -1,6 +1,7 @@
 // RestTemplateConfig.java
 package com.services.group4.permission.config;
 
+import com.services.group4.permission.Auth0Interceptor;
 import com.services.group4.permission.CorrelationIdInterceptor;
 import java.util.ArrayList;
 import java.util.List;
@@ -13,10 +14,11 @@ import org.springframework.web.client.RestTemplate;
 public class RestTemplateConfig {
 
   @Bean
-  public RestTemplate restTemplate() {
+  public RestTemplate restTemplate(Auth0Interceptor auth0Interceptor) {
     RestTemplate restTemplate = new RestTemplate();
     CorrelationIdInterceptor correlationIdInterceptor = new CorrelationIdInterceptor();
     List<ClientHttpRequestInterceptor> interceptors = new ArrayList<>();
+    interceptors.add(auth0Interceptor);
     interceptors.add(correlationIdInterceptor);
     restTemplate.setInterceptors(interceptors);
 
